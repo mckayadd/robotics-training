6fb7e2d5a074beed28793ee3f9771b42d2b3a157
mckayadd <mckaya@ceng.metu.edu.tr>
2026-04-11 23:00:57 +0200

    Add initial console app structure

---
+++ src/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RoboticsTraining
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("RoboticsTraning App v0.1");
+        }
+    }
+}

bff4fc8612fbd03dd4acd528d261938677fa3542
mckayadd <mckaya.swe@gmail.com>
2026-04-12 13:45:14 +0200

    Feature/add unit tests (#7)
    * Add unit tests and CI pipeline
    
    Closes #3
    
    * Update build.yaml
    
    * Trigger CI

---
+++ tests/RobotTests.cs
@@ -0,0 +1,33 @@
+﻿
+namespace RoboticsTraining.Tests;
+
+public class RobotTests
+{
+    [Fact]
+    public void RobotPositionTest()
+    {
+        // Arrange, Act
+        Robot testRobot = new Robot("Test", new Position(1.0, 2.0, 3.0));
+        
+        // Assert
+        Assert.Equal(1.0, testRobot.CurrentPosition.X);
+        Assert.Equal(2.0, testRobot.CurrentPosition.Y);
+        Assert.Equal(3.0, testRobot.CurrentPosition.Z);
+    }
+
+    [Fact]
+    public void RobotMovingTest()
+    {
+        // Arrange
+        Robot testRobot = new Robot("Test", new Position(1.0, 2.0, 3.0));
+        
+        // Act
+        testRobot.MoveTo(new Position(4.0, 5.0, 6.0));
+
+        // Assert
+        Assert.Equal(4.0, testRobot.CurrentPosition.X);
+        Assert.Equal(5.0, testRobot.CurrentPosition.Y);
+        Assert.Equal(6.0, testRobot.CurrentPosition.Z);
+    }
+
+}

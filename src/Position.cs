800e2eab08a83ec9bb7b071eb30386a7228205fa
mckayadd <mckaya.swe@gmail.com>
2026-04-12 08:57:09 +0200

    Add Robot and Position classes (#6)
    * Add Robot and Position classes
    
    Closes #2
    
    * Address reviews
    
    * Address review: readonly struct and private setter

---
+++ src/Position.cs
@@ -0,0 +1,15 @@
+public readonly struct Position
+{
+    public double X { get; } 
+    public double Y { get; }
+    public double Z { get; }
+
+    public Position(double x, double y, double z)
+    {
+        X = x;
+        Y = y;
+        Z = z;
+    }
+
+    public override string ToString() => $"({X}, {Y}, {Z})"; // expression-bodied member
+}
--- src/Program.cs
+++ src/Program.cs
@@ -7,5 +7,10 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine($"RoboticsTraning App v0.1 - {DateTime.Now:yyyy-MM-dd}");
+
+        Robot cleanerRobot = new Robot("Cleaner", new Position(2.1, 3.2, 1.4));
+        Console.WriteLine(cleanerRobot);
+        cleanerRobot.MoveTo(new Position(2.5, 3.2, 1.4));
+        Console.WriteLine(cleanerRobot);
     }
 }

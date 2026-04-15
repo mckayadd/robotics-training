d334ddf9c22528057aac92b65ffe1d526a81e40b
mckayadd <mckaya.swe@gmail.com>
2026-04-15 11:12:30 +0200

    Add WPF project (#9)

---
+++ RoboticsTraining.WPF/RoboticsTraining.WPF/App.xaml.cs
@@ -0,0 +1,24 @@
+﻿using RoboticsTraining;
+using RoboticsTraining.WPF.ViewModels;
+using RoboticsTraining.WPF.Views;
+using System.Windows;
+
+namespace RoboticsTraining.WPF
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            var viewModel = new MainViewModel("IRB 120", new Position(1.0, 2.0, 3.0));
+            var mainWindow = new MainWindow();
+            mainWindow.DataContext = viewModel;
+            mainWindow.Show();
+        }
+    }
+
+}

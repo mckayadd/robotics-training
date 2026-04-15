4c577a5920575ccf84c8574cb7e19e2fb5e4b179
mckayadd <mckaya@ceng.metu.edu.tr>
2026-04-15 11:07:04 +0200

    Add WPF project

---
+++ RoboticsTraining.WPF/RoboticsTraining.WPF/Commands/RelayCommand.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+
+namespace RoboticsTraining.WPF.Commands
+{
+    public class RelayCommand : ICommand
+    {
+        public event EventHandler? CanExecuteChanged;
+
+        private Action _action;
+        private Func<bool>? _canExecute;
+
+        public void Execute(object? parameter)
+        {
+            _action.Invoke();
+        }
+
+        public bool CanExecute(object? parameter)
+        {
+            return _canExecute == null || _canExecute.Invoke();
+        }
+
+        public RelayCommand(Action action)
+        { 
+            _action = action; 
+            _canExecute = null;
+        }
+
+        public RelayCommand(Action action, Func<bool>? canExecute)
+        {
+            _action = action;
+            _canExecute = canExecute;
+        }
+    }
+}
---
+++ RoboticsTraining.WPF/RoboticsTraining.WPF/ViewModels/MainViewModel.cs
@@ -0,0 +1,55 @@
+﻿using RoboticsTraining;
+using RoboticsTraining.WPF.Commands;
+using System.Windows.Input;
+
+namespace RoboticsTraining.WPF.ViewModels
+{
+    public class MainViewModel : BaseViewModel
+    {
+        public Robot Robot { get; set; }
+
+        private string _robotName;
+
+        public string RobotName
+        {
+            get => _robotName; 
+            set 
+            { 
+                _robotName = value; 
+                OnPropertyChanged();
+            }
+        }
+
+        private Position _robotPosition;
+
+        public Position RobotPosition
+        {
+            get => _robotPosition;
+            set 
+            { 
+                _robotPosition = value; 
+                OnPropertyChanged();
+            }
+        }
+
+        public ICommand MoveRobotCommand { get;  }
+
+        public MainViewModel(String  robotName,  Position robotPosition) 
+        {
+            _robotName = robotName;
+            _robotPosition = robotPosition;
+            Robot = new Robot(_robotName, _robotPosition);
+            MoveRobotCommand = new RelayCommand(
+                    () => MoveRobot(new Position(4.0, 5.0, 6.0))
+                );
+        }
+
+        public void MoveRobot(Position newPosition)
+        { 
+            Robot.MoveTo(newPosition);
+            RobotName = Robot.Name;
+            RobotPosition = Robot.CurrentPosition;
+        }
+
+    }
+}
